f03f3302e4e8cd896d1a8ceab417037742efe63a
Gray Sanity <graysanitycdm@gmail.com>
2021-08-14 22:46:34 +0200

    maybe this works

---
+++ server.js
@@ -0,0 +1,31 @@
+/*Dependencies*/
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+/*Port number for webserver*/
+const port = 3000;
+
+/*Set server, app and socket.io up*/
+var app = express();
+var server = http.Server(app);
+
+/*Assign port number to app*/
+app.set('port', port);
+/*Make ./static be available*/
+app.use('/', express.static(__dirname + '/'));
+
+/*On a request, send index.html over*/
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+/*404 screen*/
+app.get('*', function(req, res){
+    res.status(404).send('<head><style>body {height: 98vh; background-color: black; display: flex; justify-content: center; align-items: center;}</style><title>That is an error</title></head><body><img width="70%"; src="//i.imgur.com/hMfpDQ9.png"></body>');
+});
+
+/*End of startup*/
+server.listen(port, function(){
+    console.log(`Started 5eTools server on port  ${port}.`);
+});
